42c60af7b24c335e6e10eb6892a09b766ed2fc58
Tykhonchuk <tykhonchukivan@gmail.com>
2021-01-14 23:21:39 +0200

    feat: func theory

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const chai = require("chai");
 const assert = chai.assert;
 
-const basics = require('./src/basic_theory/basics').basics
+const basics = require('./src/basic_theory/1_basics').basics
+const func = require('./src/basic_theory/2_func').func
 
 describe('Basics js theory', function () {
   it('Number addition ', function () {
@@ -58,96 +59,109 @@ describe('Basics js theory', function () {
     const act = basics.parse.parsStringToNumber(num);
     assert.equal(exp, typeof (act));
   })
-  it('Grade A', function (){
+  it('Grade A', function () {
     const exp = 'A';
     const num = 90;
     const act = basics.conditions.getStudentGrade(num);
     assert.equal(exp, act);
   })
-  it('Grade B',function (){
+  it('Grade B', function () {
     const exp = 'B';
     const num = 70;
     const act = basics.conditions.getStudentGrade(num);
     assert.equal(exp, act);
   })
-  it('Grade C',function (){
+  it('Grade C', function () {
     const exp = 'C';
     const num = 40;
     const act = basics.conditions.getStudentGrade(num);
     assert.equal(exp, act);
   })
-  it('Grade C Ternary',function (){
+  it('Grade C Ternary', function () {
     const exp = 'C';
     const num = 40;
     const act = basics.conditions.getStudentGradeTernary(num);
     assert.equal(exp, act);
   })
-  it('Grade False',function (){
+  it('Grade False', function () {
     const exp = '';
     const num = null;
     const act = basics.conditions.getStudentGrade(num);
     assert.equal(exp, act);
   })
-  it('Get settings users switch admin', function (){
+  it('Get settings users switch admin', function () {
     const exp = 'admin';
     const arg = 'admin';
     const act = basics.conditions.getSettingsUsersSwitch(arg).admin;
     assert.equal(exp, act);
   })
-  it('Get settings users switch user', function (){
+  it('Get settings users switch user', function () {
     const exp = 'user';
     const arg = 'user';
     const act = basics.conditions.getSettingsUsersSwitch(arg).user;
     assert.equal(exp, act);
   })
-  it('Get settings users switch false', function (){
+  it('Get settings users switch false', function () {
     const exp = false;
     const arg = '123';
     const act = basics.conditions.getSettingsUsersSwitch(arg);
     assert.equal(exp, act);
   })
-  it('Add char loop string while', function (){
+  it('Add char loop string while', function () {
     const exp = '11111';
     const arg = '1';
     const act = basics.loop.addCharLoop(arg);
     assert.equal(exp, act);
   })
-  it('Add char loop string do while', function (){
+  it('Add char loop string do while', function () {
     const exp = '111';
     const arg = '1';
     const argNum = 3;
     const act = basics.loop.addCharLoopDo(arg, argNum);
     assert.equal(exp, act);
   })
-  it('Add char loop string do while -arg', function (){
+  it('Add char loop string do while -arg', function () {
     const exp = '1';
     const arg = '1';
     const argNum = -3;
     const act = basics.loop.addCharLoopDo(arg, argNum);
     assert.equal(exp, act);
   })
-  it('Add char loop string do for break', function (){
+  it('Add char loop string do for break', function () {
     const exp = 'QWER';
     const arg = 'QWERTY';
     const arg1 = 4;
     const arg2 = true;
     const arg3 = null;
     const arg4 = null;
-    const act = basics.loop.addCharFor(arg,arg1,arg2,arg3,arg4);
+    const act = basics.loop.addCharFor(arg, arg1, arg2, arg3, arg4);
     assert.equal(exp, act);
   })
-  it('Add char loop string do for continue', function (){
+  it('Add char loop string do for continue', function () {
     const exp = 'QWRTY';
     const arg = 'QWERTY';
     const arg1 = null;
     const arg2 = null;
     const arg3 = 2;
     const arg4 = true;
-    const act = basics.loop.addCharFor(arg,arg1,arg2,arg3,arg4);
+    const act = basics.loop.addCharFor(arg, arg1, arg2, arg3, arg4);
     assert.equal(exp, act);
   })
+})
 
-
-
-
+describe('Function js theory', function () {
+  it('Get full name', function () {
+    const exp = 'Ivan Tychonchuk';
+    const firstName = 'Ivan';
+    const lastName = 'Tychonchuk';
+    const act = func.getFullName(firstName, lastName);
+    assert.equal(exp, act);
+  })
+  it('Get full name false', function () {
+    const exp = false;
+    const firstName = 'Ivan';
+    const lastName = 5;
+    const act = func.getFullName(firstName, lastName);
+    assert.equal(exp, act);
+  })
 })
