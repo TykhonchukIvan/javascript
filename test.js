ca36b614f7267fddea61a472aa326d6e07cb3ccf
Tykhonchuk <tykhonchukivan@gmail.com>
2021-01-17 20:46:15 +0200

    feat: Closures

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const basicsJsTheoryTest = require('./src/basic-theory/test/1_basics-test').basicsJsTheoryTest;
 const functionJsTheoryTest = require('./src/basic-theory/test/2_func-test').functionJsTheoryTest;
 const arrayAndObjectJsTheory = require('./src/basic-theory/test/3_arrayAndObject-test').arrayAndObjectJsTheory;
+const closuresJsTheoryTest = require('./src/basic-theory/test/4_closures-test').closuresJsTheoryTest
 
 basicsJsTheoryTest()
 functionJsTheoryTest()
 arrayAndObjectJsTheory()
+closuresJsTheoryTest()
